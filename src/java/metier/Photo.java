23d5bad291f9f694e131dd07fdb75a319eb67ec8
Daryl Issoufi <daryl.issoufi@gmail.com>
2019-01-14 11:02:23 +0100

    First commit

---
+++ src/java/metier/Photo.java
@@ -0,0 +1,42 @@
+package metier;
+
+public class Photo {
+    private int id;
+    private String name;
+    private Category category;
+    private String path;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+    
+    
+}
